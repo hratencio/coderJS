57cf41f290f57b007afb4f7a4401536eb883a1f5
Hugo Atencio <hugo@192.168.1.5>
2021-06-09 23:35:01 -0300

    js

---
+++ main.js
@@ -0,0 +1,6 @@
+function usuario(){
+    let nombre = prompt('Cual es tu nombre?');
+    let apellido = prompt('Caul es tu apellido?');
+    alert(`Hola ${nombre} ${apellido}, saludos!`);
+}
+usuario();
