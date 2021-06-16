679b505333aacb262b06d110df64bd03057b0eac
Hugo Atencio <hugo@192.168.1.5>
2021-06-16 13:41:30 -0300

    segundo desafío

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function usuario(){
+/*function usuario(){
     let nombre = prompt('Cual es tu nombre?');
     let apellido = prompt('Caul es tu apellido?');
     alert(`Hola ${nombre} ${apellido}, saludos!`);
 }
-usuario();
+usuario();*/ 
+
+function edad(){
+    let edad = prompt('Cual es tu edad?');
+    if(edad >= 18){
+        console.log('Bienvenido, puedes ingresar')
+    } else if (edad < 18){
+        console.log('Lo siento... necesitas autorización')}
+    }
