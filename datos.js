f6e200b4ce761e84bcf4bcfbfece47c13f27e4e2
Hugo Atencio <hugo@192.168.1.4>
2021-08-22 16:52:49 -0300

    final push

---
+++ datos.js
@@ -0,0 +1,86 @@
+const bicis = [
+    {
+        "id": 1,
+        "nombre": "TOP FUEL 9.9",
+        "descripcion": "La Top Fuel 9.9 es nuestra mejor bici de montaña doble suspensión para corredores de resistencia y ciclistas multidisciplina.",
+        "precio": 4500,
+        "img": "http://www.trekbikesargentina.com/2020/Top%20Fuel%209.9%20Cian%20Black.jpg"
+    },
+    {
+        "id":2,
+        "nombre": "TOP FUEL 8",
+        "descripcion": "La Top Fuel 8 combina la eficacia y ligereza de una bicicleta de cross country y la capacidad de una bicicleta de trail.",
+        "precio": 3000,
+        "img": "http://www.trekbikesargentina.com/trek-bicis-mtb/assets/images/TopFuel8NX-GB-2.jpg"
+    },
+    {
+        "id": 3,
+        "nombre": "PRO CALIBER 9.5",
+        "descripcion": "La Procaliber 9.5 es una bicicleta rígida de carbono ligera con la ventaja exclusiva de nuestra rótula IsoSpeed que absorbe los baches de los senderos",
+        "precio": 1200,
+        "img": "http://www.trekbikesargentina.com/trek-bicis-mtb/assets/images/Procaliber95-YR-2.jpg"
+    },
+    {
+        "id": 4,
+        "nombre": "X-CALIBER 7",
+        "descripcion": "La X-Caliber 7 es el punto de entrada perfecto para rápidos paseos y carreras de cross country.",
+        "precio": 800,
+        "img": "http://www.trekbikesargentina.com/trek-bicis-mtb/assets/images/XCaliber7-OG-2.jpg"
+    },
+    {
+        "id": 5,
+        "nombre": "MARLIN 7",
+        "descripcion": "La Marlin 7 es en donde comienzan las bicis de montaña dignas para la carrera a la par de nuestras bicis de carrera de gama más alta.",
+        "precio": 600,
+        "img": "http://www.trekbikesargentina.com/trek-bicis-mtb/assets/images/Marlin7-BL-2.jpg"
+    },
+    {
+        "id": 6,
+        "nombre": "SUPERCALIBER 9.9",
+        "descripcion": "La Supercaliber 9.9 es la bici de carrera cross country más rápida, ligera y capaz que hacemos, y es justamente por eso que es la bici elegida por Trek Factory Racing para la Copa Mundial.",
+        "precio": 7000,
+        "img": "http://www.trekbikesargentina.com/2020/Supercaliber%209.9%20Cian%20Black.jpg"
+    },
+    {
+        "id": 7,
+        "nombre": "FUEL EX 7",
+        "descripcion": "La Fuel EX 7 destaca en los descensos más complicados donde su suspensión actualizada se hace notar.",
+        "precio": 9000,
+        "img": "http://www.trekbikesargentina.com/trek-bicis-mtb/assets/images/FuelEX7_BK-OG-2.jpg"
+    },
+    {
+        "id": 8,
+        "nombre": "MADONE SL 6 Disc",
+        "descripcion": "La Madone SL Disc es una bicicleta de carretera preparada para competición que ofrece increíbles resultados en aerodinámica y capacidad de manejo.",
+        "precio": 8000,
+        "img": "http://www.trekbikesargentina.com/2020/Madone%20SL%206%20Disc%20Black%20Red.jpg"
+    },
+    {
+        "id": 9,
+        "nombre": "EMONDA ALR 6",
+        "descripcion": "La Émonda ALR 4 es una bici de carrera de aluminio ligera con un valor increíble.",
+        "precio": 1000,
+        "img": "http://www.trekbikesargentina.com/2020/Emonda%20ALR%204%20disc.jpg"
+    },
+    {
+        "id": 10,
+        "nombre": "EMONDA ALR 4",
+        "descripcion": "La Émonda ALR 4 es una bici de carrera de aluminio ligera con un valor increíble. El cuadro Émonda ALR Disc te ofrece la apariencia y el rendimiento del carbono al precio de la aleación, que es mucho más bajo.",
+        "precio": 700,
+        "img": "http://www.trekbikesargentina.com/2020/Emonda%20ALR%204%20Red-Black.jpg"
+    },
+    {
+        "id": 11,
+        "nombre": "DUAL SPORT",
+        "descripcion": "La Dual Sport 1 es una bicicleta híbrida de verdad: rinde en una gran variedad de superficies y está preparada para rodar por terrenos pavimentados o por senderos fáciles por igual.",
+        "precio": 400,
+        "img": "http://www.trekbikesargentina.com/trek-bicis-mtb/assets/images/DualSport1-BK-2.jpg"
+    },
+    {
+        "id": 12,
+        "nombre": "VERVE 1 DISC",
+        "descripcion": "La Verve 1 es una bici híbrida introductoria diseñada para darte comodidad en los paseos recreativos. Tiene un cuadro de aluminio ligero, un asiento acolchonado, llantas anchas para una estabilidad extra y 21 velocidades.",
+        "precio": 250,
+        "img": "http://www.trekbikesargentina.com/2020/verve%201CH.jpg"
+    }
+]
--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-
-function usuario(){
-    let nombre = prompt('Cual es tu nombre');
-    let apellido = prompt('Caul es tu apellido?');
-    alert(`Hola, ${nombre} ${apellido} Bienvenidos a Baires Bikes!`);
-}
-
-
-class Bicicleta{
-    constructor(nombre, marca, modelo, precio){
+class Bicicletas{
+    constructor(id, nombre, descripcion, precio, img){
+        this.id = id;
         this.nombre = nombre;
-        this.marca = marca;
-        this.modelo = modelo;
+        this.descripcion = descripcion;
         this.precio = precio;
+        this.img = img;
     }
     precioFinal(){
         let iva = this.precio * 0.21;
@@ -20,71 +13,79 @@ class Bicicleta{
     }
 }
 
-let bici1 = new Bicicleta('StumpJumper','Specialized','Trail',3500);
-let bici2 = new Bicicleta('HighTower','SantaCruz','Trail', 4500);
-let bici3 = new Bicicleta('Rockhopper','Specialized','CrossCountry', 1000);
-let bici4 = new Bicicleta('Epic','Specialized','CrossCountry', 4000);
-let bici5 = new Bicicleta('Bronson','SantaCruz','Trail', 2000);
-let bici6 = new Bicicleta('Session','Trek','Trail', 2500);
-let bici7 = new Bicicleta('TCR','Giant','Carretera', 1500);
-let bici8 = new Bicicleta('Emonda','Trek','Carretera', 2000);
-let bici9 = new Bicicleta('Tarmac','Specialized','Carretera', 1500);
-let bici10 = new Bicicleta('Slash','Trek','Trail', 3000);
-let bici11 = new Bicicleta('Chisel','Specialized','CrossCountry', 1600);
-let bici12 = new Bicicleta('Marlin','Trek','CrossCountry',1200);
-
-const Bicicletas = [];
-Bicicletas.push(bici1);
-Bicicletas.push(bici2);
-Bicicletas.push(bici3);
-Bicicletas.push(bici4);
-Bicicletas.push(bici5);
-Bicicletas.push(bici6);
-Bicicletas.push(bici7);
-Bicicletas.push(bici8);
-Bicicletas.push(bici9);
-Bicicletas.push(bici10);
-Bicicletas.push(bici11);
-Bicicletas.push(bici12);
-
-const MTB = Bicicletas.filter(Bicicleta => Bicicleta.modelo == 'Trail');
-console.log(MTB);
-const CC = Bicicletas.filter(Bicicleta => Bicicleta.modelo == 'CrossCountry');
-console.log(CC);
-const Ruta = Bicicletas.filter(Bicileta => Bicileta.modelo == 'Carretera');
-console.log(Ruta);
-const PreciosCrecientes = Bicicletas.sort(function(a,b){
-    return a.precio - b.precio;
-})
-
-
-const Decrecientes = Bicicletas.sort(function(a,b){
+// 
+const Decrecientes = bicis.sort(function(a,b){
     return b.precio - a.precio;
 })
 
-for (const bicis of Bicicletas){
-    $('#todasBicis').prepend(`<div id="grid1">
-    <h3> ${bicis.nombre} </h3>
-   <p> Marca: ${bicis.marca} </p>
-   <p> Modelo: ${bicis.modelo} </p>
-   <p> Precio: ${bicis.precio}</p>
-   <button id="boton"> Comprar </button>
-   </div>`);
+bicis.forEach(e => {
+    $('#elemento3').prepend(`
+    <div id="cartas">
+        <h2>${e.nombre}</h2>
+        <img id="imagenesCartas" src="${e.img}" alt="" srcset="">
+         <p>${e.descripcion}</p>
+             <div id="botonesCartas">
+                <h3>Precio: ${e.precio}$</h3>
+                <button id="selectorBici">Comprar</button>
+            </div>
+     </div>`)
+    $("#selectorBici").on('click', function(bicis){
+        bicis
+        let costoE = 200;
+        $("#biciNombre").val(`${e.nombre}`)
+        $("#precio").val(`${e.precio}$`);
+        $("#costosEnvio").val(`${costoE}$`);
+        let subtotal = e.precio + costoE;
+        $("#subtotal").val(`${subtotal}$`);
+     $("#botonConfirmar").on('click', function(bicis){
+            bicis
+            let iva = e.precio * 0.15
+            let total = e.precio + iva;
+            $("table").show("fast");
+            $("#tableNombre").text(`${e.nombre}`);
+            $("#tablaImg").append(`<img id="tableImg" src="${e.img}" alt="">`);
+            $("#tableSubtotal").append(`<p>${subtotal}</p>`);
+            $("#tableTotal").append(`<p>${total}</p>`);
+            $("#anuncioI").show("fast");
+            $("#borrarImg").on('click', function(){
+                $("table").hide();
+            })
+        })
+    });
+});
 
-   $("#boton").on('click', function(){
-       console.log(`Agregaste ${bicis.nombre} al carrito`);
-   });
+$("#botonE").on('click', function(){
+    $("#botonR").hide();
+})
+$("#botonR").on('click', function(){
+    $("#botonE").hide();
+    $("#direccion").hide();
+})
+$("#botonConfirmar").on('click', function(){
+    $("#elemento5").show("fast");
+})
+function datosUsuario(){
+    let nombre = document.getElementById("nombreApellido").value;
+    let direccion = document.getElementById("direccion").value;
+    let numero = document.getElementById("numeroTelefono").value;
+    console.log(nombre,direccion,numero);
+   if(nombre == "" && direccion == "" && numero == ""){
+       $("#elemento6").hide();
+   }else{ 
+       $("#elemento6").append(`
+        <div>
+            <div id="confirmacionNombre">
+                <h2>Felicitaciones ${nombre}!</H2>
+            </div>
+            <div id="confirmacionDN">
+                <p>Tu bicicleta sera entregada en la direccion: ${direccion}.</p>
+                <p>Y sera confirmada la entrega a traves del siguiente numero: ${numero}</p>
+                <p>Disfruta el descenzo sin compromisos!</p>
+                <img src="https://media2.giphy.com/media/d7o7xi94fQ2vDTaeXk/giphy.gif" alt="" srcset="">
+            </div>
+    </div>`)
+   }
 }
-/*Bicicletas.forEach(function(bicis){
-    let nombreBici1 = document.getElementById('todasBicis');
-    nombreBici1.innerHTML += `
-    <div id="grid1">
-    <h3> ${bicis.nombre} </h3>
-   <p> Marca: ${bicis.marca} </p>
-   <p> Modelo: ${bicis.modelo} </p>
-   <p> Precio: ${bicis.precio}</p>
-   <button id="boton"> Comprar </button>
-   </div>
-   `;
-})*/
+
+
 
